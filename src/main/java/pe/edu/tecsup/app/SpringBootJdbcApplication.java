b4c29f0febacda77478d76af4afb1d094212b7e0
jgomezz <jgomezz@gmail.com>
2026-01-11 11:06:47 -0500

    init project

---
+++ src/main/java/pe/edu/tecsup/app/SpringBootJdbcApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJdbcApplication.class, args);
+	}
+
+}
