09d8d01a472b02d3a7ab7ade2d05ea1698c4206f
jgomezz <jgomezz@gmail.com>
2026-01-11 12:46:42 -0500

    setting jdbcTemplate

--- src/main/java/pe/edu/tecsup/app/repositories/CategoriaRepositoryImpl.java
+++ src/main/java/pe/edu/tecsup/app/repositories/CategoriaRepositoryImpl.java
@@ -1,27 +1,54 @@
 package pe.edu.tecsup.app.repositories;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import pe.edu.tecsup.app.entities.Categoria;
 
+import java.sql.ResultSet;
 import java.util.List;
+
+/**
+ * Mapeo de los resultados de la consulta SQL a la entidad Categoria
+ */
+class CategoriaRowMapper implements RowMapper<Categoria> {
+
+    @Override
+    public Categoria mapRow(ResultSet rs, int rowNum) throws java.sql.SQLException {
+        Categoria categoria =  Categoria.builder()
+                                        .id(rs.getLong("id"))
+                                        .nombre(rs.getString("nombre"))
+                                        .orden(rs.getInt("orden"))
+                                        .build();
+        return categoria;
+    }
+}
 
 @Slf4j
 @Repository
+@AllArgsConstructor
 public class CategoriaRepositoryImpl  implements CategoriaRepository{
+
+    private final JdbcTemplate jdbcTemplate;
 
     @Override
     public List<Categoria> findAll() {
 
         log.info("findAll categorias - repository");
 
-        List<Categoria> categorias = List.of(
-                Categoria.builder().id(1L).nombre("Tecnología").orden(1).build(),
-                Categoria.builder().id(2L).nombre("Hogar").orden(2).build(),
-                Categoria.builder().id(3L).nombre("Deportes").orden(3).build()
-        );
-
+        String sql = """
+                    SELECT id, nombre, orden 
+                    FROM categorias 
+                    ORDER BY orden
+                """;
+        List<Categoria> categorias
+                = this.jdbcTemplate.query(
+                                            sql,
+                                            new CategoriaRowMapper()
+                                        );
 
         return categorias;
     }
