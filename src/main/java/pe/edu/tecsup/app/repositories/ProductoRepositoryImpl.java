5f986ea1cad8a639e1bd13c8a255fc0b3edd8189
jgomezz <jgomezz@gmail.com>
2026-01-18 08:19:41 -0500

    add Product Repository

--- src/main/java/pe/edu/tecsup/app/repositories/ProductoRepositoryImpl.java
+++ src/main/java/pe/edu/tecsup/app/repositories/ProductoRepositoryImpl.java
@@ -1,13 +1,73 @@
 package pe.edu.tecsup.app.repositories;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import pe.edu.tecsup.app.entities.Categoria;
+import pe.edu.tecsup.app.entities.Producto;
 
+import java.util.List;
+
+/**
+ *
+ */
+class ProductoRowMapper implements RowMapper<Producto> {
+    @Override
+    public Producto mapRow(java.sql.ResultSet rs, int rowNum) throws java.sql.SQLException {
+        Producto producto =  Producto.builder()
+                                        .id(rs.getLong("id"))
+                                        .categorias_id(rs.getLong("categorias_id"))
+                                        .nombre(rs.getString("nombre"))
+                                        .descripcion(rs.getString("descripcion"))
+                                        .precio(rs.getDouble("precio"))
+                                        .stock(rs.getInt("stock"))
+                                        .imagen_nombre(rs.getString("imagen_nombre"))
+                                        .imagen_tipo(rs.getString("imagen_tipo"))
+                                        .imagen_tamanio(rs.getLong("imagen_tamanio"))
+                                        .estado(rs.getInt("estado"))
+                                        .creado(rs.getDate("creado"))
+                                        .build();
+        return producto;
+    }
+}
+
+
+@Slf4j
 @Repository
 @AllArgsConstructor
 public class ProductoRepositoryImpl implements ProductoRepository{
 
     private final JdbcTemplate jdbcTemplate;
 
+    @Override
+    public List<Producto> findAll() throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public List<Producto> findByName(String nombre) throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public Producto findById(Long id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void save(Producto producto) throws Exception {
+
+    }
+
+    @Override
+    public void update(Long id, String nombreProducto) throws Exception {
+
+    }
+
+    @Override
+    public void deleteById(Long id) throws Exception {
+
+    }
 }
